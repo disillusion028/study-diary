affb8cc90b9915280c93a6e63e3162111b7db8f5
曹启 <905561651@qq.com>
2017-07-28 20:13:15 +0800

    728

--- c++learning/sz.c
+++ c++learning/sz.c
@@ -1,5 +1,28 @@
-//#include <stdlib.h>
-static char buf[1024000];
-int main(){
-    return 0;
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define NUMPROC 5
+
+int main(void)
+{
+  pid_t child[NUMPROC];
+  int status[NUMPROC];
+  int i;
+
+  printf("parent = %d\n", getpid());
+
+  for(i=0;i<NUMPROC;++i) {
+    if(fork() == 0) {
+      sleep(i);
+      printf("i=%d, %d\n",i, getpid());
+      _exit(0);
+    }
+  }
+
+  for(i=0;i<NUMPROC;++i)
+    child[i] = wait(&status[i]);
+
+  for(i=0;i<NUMPROC;++i)
+    printf("Exit = %d, child = %d\n", WEXITSTATUS(status[i]), child[i]);
 }
